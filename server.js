26ab839f6be3658642570c76b2e775502e24b51d
Nikola Maksimovic <nikola.maksimovic.jobs@gmail.com>
2025-03-03 23:17:21 +0100

    Generated sample node app and set up the docker.

---
+++ server.js
@@ -0,0 +1,15 @@
+import { createServer } from 'node:http';
+
+// Make the server accessible from outside the container
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
